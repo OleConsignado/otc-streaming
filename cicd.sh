b5853a35ace393e3c733e4a6b693b472fa5f4745
Matheus Neder <matheusneder@gmail.com>
2018-05-29 13:55:58 -0300

    Update cicd.sh

--- cicd.sh
+++ cicd.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-export CICD_COMMON_REVISION=3acb270e4000bf455519b4f5cc912805fcbcdaff
+CICD_COMMON_VERSION="v1.0"
+
 export CLASS_LIBRARY_PROJ_DIR=Source/Otc.Streaming
 export TEST_PROJ_DIR=Source/Otc.Streaming.Tests
 
 cd $TRAVIS_BUILD_DIR
 
-wget -q https://raw.githubusercontent.com/OleConsignado/otc-cicd-common/$CICD_COMMON_REVISION/cicd-common.sh -O ./cicd-common.sh
+wget -q https://raw.githubusercontent.com/OleConsignado/otc-cicd-common/$CICD_COMMON_VERSION/cicd-common.sh -O ./cicd-common.sh
 chmod +x ./cicd-common.sh
+
 ./cicd-common.sh $@
