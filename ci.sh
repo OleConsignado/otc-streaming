c0aad5983288eb4cfced52ecd0885c12f89f0f66
Matheus Neder <matheusneder@gmail.com>
2018-05-29 11:24:22 -0300

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -2,47 +2,11 @@
 
 set -e
 
-CLASS_LIBRARY_PROJ_DIR=$TRAVIS_BUILD_DIR/Source/Otc.Streaming
-TEST_PROJ_DIR=$TRAVIS_BUILD_DIR/Source/Otc.Streaming.Tests
+export CLASS_LIBRARY_PROJ_DIR=Source/Otc.Streaming
+export TEST_PROJ_DIR=Source/Otc.Streaming.Tests
 
-function install
-{
-	cd $CLASS_LIBRARY_PROJ_DIR
-	dotnet restore
-}
+cd $TRAVIS_BUILD_DIR
+wget https://raw.githubusercontent.com/OleConsignado/otc-cicd-common/3acb270e4000bf455519b4f5cc912805fcbcdaff/cicd-common.sh
+chmod +x ./cicd-common.sh
 
-function build
-{
-	cd $CLASS_LIBRARY_PROJ_DIR
-	dotnet build -c Release
-}
-
-function deploy
-{
-	cd $CLASS_LIBRARY_PROJ_DIR
-	ARTIFACTS_FOLDER=./artifacts
-
-	if [ ! -d $ARTIFACTS_FOLDER ]
-	then
-		mkdir $ARTIFACTS_FOLDER
-	fi
-
-	if [[ ${TRAVIS_BRANCH^^} = *"ALPHA"* ]] || [[ ${TRAVIS_BRANCH^^} = *"BETA"* ]]
-	then
-		SUFFIX=$(echo $TRAVIS_BRANCH-build$TRAVIS_BUILD_NUMBER | sed 's/[^0-9A-Za-z-]//g')
-		SUFFIX_ARG="--version-suffix=$SUFFIX"
-	fi
-
-	dotnet pack -c Release $SUFFIX_ARG -o $ARTIFACTS_FOLDER
-	dotnet nuget push --api-key $NUGET_API_KEY $ARTIFACTS_FOLDER/*.nupkg --source $NUGET_ENDPOINT # https://api.nuget.org/v3/index.json
-
-	rm -Rf $ARTIFACTS_FOLDER
-}
-
-function test
-{
-	cd $TEST_PROJ_DIR
-	dotnet test
-}
-
-$@
+./cicd-common.sh $@
